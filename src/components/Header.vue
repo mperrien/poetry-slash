f2ba870944c40047f5f07e9f7ca6325f22d2124e
Marc Perrien <marc@chaosiq.io>
2020-03-08 22:30:29 +0100

    Basic settings. Header component.
    Signed-off-by: Marc Perrien <marc@chaosiq.io>

---
+++ src/components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+  <header class="header">
+    <img src="../assets/images/twitter-poem-generator.png" alt="" />
+    <h1>Twitter Poem Generator</h1>
+  </header>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Header extends Vue {}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+.header {
+  padding: 2em 1em;
+
+  background-color: $yellow;
+
+  text-align: center;
+  h1 {
+    margin: 0;
+
+    color: $violet;
+    font-size: 2em;
+  }
+  img {
+    display: block;
+    margin: 0 auto 1em;
+    height: auto;
+    width: 10em;
+    max-width: 80vw;
+  }
+}
+
+</style>
